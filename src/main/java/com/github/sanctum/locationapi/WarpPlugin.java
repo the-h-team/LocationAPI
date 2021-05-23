30a3a6dd2c293a55d26ba81a45cc69de5bffe0da
Hempfest <64885225+Hempfest@users.noreply.github.com>
2021-05-23 00:49:57 -0700

    * Ready for initial testing.

---
+++ src/main/java/com/github/sanctum/locationapi/WarpPlugin.java
@@ -0,0 +1,18 @@
+package com.github.sanctum.locationapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WarpPlugin extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public void onEnable() {
+
+		getLogger().info("- LocationAPI services now available.");
+
+	}
+}
